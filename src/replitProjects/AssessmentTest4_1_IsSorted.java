d3eac8e283870c6af3943b5043e91f601e5ebec4
Dervis B Uludag <55367496+blackpars4x4@users.noreply.github.com>
2020-12-16 19:49:34 -0800

    Dec16-4

---
+++ src/replitProjects/AssessmentTest4_1_IsSorted.java
@@ -0,0 +1,55 @@
+package replitProjects;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.sort;
+
+public class AssessmentTest4_1_IsSorted {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(isSort(arr));
+
+    }
+    public static boolean isSort(int[] nums) {
+
+
+         int[] temp = nums;
+         Arrays.sort(temp);
+
+         if(nums == temp){
+             return true;
+         }else{
+             return false;
+         }
+
+    }
+}
+/*
+this is a sorted collection (in this case array):
+
+7,8,9,10
+
+this is an unsorted collection:
+
+5,4,9,8,7,3
+
+sorted in this case means smallest number to biggest.
+you will not do sorting in this assignment.
+you will only check if an array is sorted.
+
+isSort gets an array and returns true if its sorted.
+
+for example:
+isSort([2,3,5,4,9]);
+returns: false
+
+isSort([1,2,3]);
+returns: true
+
+isSort([7,8,9]);
+returns: true.
+
+hint:when you loop the array if current number in the array is greater then next number,
+array is not sorted return false.
+ */
